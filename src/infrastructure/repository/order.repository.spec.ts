c834d72e6f788ec2ba8453fdfaf5893604f5a0d9
@murilloc <murilloc@gmail.com>
2024-04-07 19:47:55 -0300

    MTodos os m©todos RoderRepository implementados e ok

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -78,5 +78,142 @@ describe('Order repository unity test', () => {
             ]
         });
     });
-});
 
+    it('should find an order', async () => {
+        // Create a customer
+        const customerRepository = new CustomerRepository();
+        const customer = new Customer('1', 'John Doe');
+        const address = new Address('Main Street', 100, '12345', 'New York');
+        customer.changeAddress(address);
+        await customerRepository.create(customer);
+
+        // Create a product
+        const productRepository = new ProductRepository();
+        const product = new Product('123', 'Product 1', 100);
+        await productRepository.create(product);
+
+        // Create an order
+        const orderItem1 = new OrderItem('1', product.name, product.price, product.id, 2);
+        const orderItem2 = new OrderItem('2', product.name, product.price, product.id, 3);
+        const orderItems = [orderItem1, orderItem2];
+
+        const order = new Order('1', customer.id, orderItems);
+        const orderRepository = new OrderRepository();
+        await orderRepository.create(order);
+
+        // Find order
+        const orderModel = await OrderModel.findOne({where: {id: order.id}, include: ["items"]});//
+        expect(orderModel.toJSON()).toStrictEqual({
+            id: '1',
+            customerId: customer.id,
+            total: order.total(),
+            items: order.items.map((item) => ({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                quantity: item.quantity,
+                productId: item.productId,
+                orderId: order.id,
+            }))
+        });
+    });
+
+    it('should return all orders', async () => {
+        // Create a customer
+        const customerRepository = new CustomerRepository();
+        const customer = new Customer('1', 'John Doe');
+        const address = new Address('Main Street', 100, '12345', 'New York');
+        customer.changeAddress(address);
+        await customerRepository.create(customer);
+
+        // Create a product
+        const productRepository = new ProductRepository();
+        const product = new Product('123', 'Product 1', 100);
+        await productRepository.create(product);
+
+        // Create an order
+        const orderItem1 = new OrderItem('1', product.name, product.price, product.id, 2);
+        const orderItem2 = new OrderItem('2', product.name, product.price, product.id, 3);
+        const orderItems = [orderItem1, orderItem2];
+
+        const order = new Order('1', customer.id, orderItems);
+        const orderRepository = new OrderRepository();
+        await orderRepository.create(order);
+
+        const orderItem3 = new OrderItem('3', product.name, product.price, product.id, 4);
+        const orderItem4 = new OrderItem('4', product.name, product.price, product.id, 5);
+        const orderItems2 = [orderItem3, orderItem4];
+
+        const order2 = new Order('2', customer.id, orderItems2);
+        await orderRepository.create(order2);
+
+        // Find all orders
+        const orders = await orderRepository.findAll();
+
+        // map don´t work with async
+        for (const order of orders) {
+            // Find order
+            const orderModel = await OrderModel.findOne({where: {id: order.id}, include: ["items"]});//
+            expect(orderModel.toJSON()).toStrictEqual({
+                id: order.id,
+                customerId: customer.id,
+                total: order.total(),
+                items: order.items.map((item) => ({
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    quantity: item.quantity,
+                    productId: item.productId,
+                    orderId: order.id,
+                }))
+            });
+        }
+    });
+
+    it('should update an order', async () => {
+        // Create a customer
+        const customerRepository = new CustomerRepository();
+        const customer = new Customer('1', 'John Doe');
+        const address = new Address('Main Street', 100, '12345', 'New York');
+        customer.changeAddress(address);
+        await customerRepository.create(customer);
+
+        // Create a product
+        const productRepository = new ProductRepository();
+        const product = new Product('123', 'Product 1', 100);
+        await productRepository.create(product);
+
+        // Create an order
+        const orderItem1 = new OrderItem('1', product.name, product.price, product.id, 2);
+        const orderItem2 = new OrderItem('2', product.name, product.price, product.id, 3);
+        const orderItems = [orderItem1, orderItem2];
+
+        const order = new Order('1', customer.id, orderItems);
+        const orderRepository = new OrderRepository();
+        await orderRepository.create(order);
+
+        // Update order
+        const orderItem3 = new OrderItem('3', product.name, product.price, product.id, 4);
+        const orderItem4 = new OrderItem('4', product.name, product.price, product.id, 5);
+        const orderItems2 = [orderItem3, orderItem4];
+
+        const updatedOrder = new Order('1', customer.id, orderItems2);
+        await orderRepository.update(updatedOrder);
+
+        // Find order
+        const orderModel = await OrderModel.findOne({where: {id: order.id}, include: ["items"]});
+        expect(orderModel.toJSON()).toStrictEqual({
+            id: order.id,
+            customerId: customer.id,
+            total: updatedOrder.total(),
+            items: updatedOrder.items.map((item) => ({
+                id: item.id,
+                name: item.name,
+                price: item.price,
+                quantity: item.quantity,
+                productId: item.productId,
+                orderId: order.id,
+            }))
+        });
+    });
+});
